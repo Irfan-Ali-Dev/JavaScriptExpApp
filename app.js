f30b2ea579d540de50c5500fe78bc43567ccb110
Irfan-Ali-Dev <yousafbuilders.it@gmail.com>
2026-01-04 10:56:16 +0500

    Files created

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello Exp");
+
+const amount = document.querySelector("#amount");
+const category = document.querySelector("#category")
+const remarks = document.querySelector("#remarks")
+const submit = document.querySelector("#submit")
+const showData = document.querySelector("#showData")
+
+// function renderData() {
+//     amount.value += showData;  
+//     console.log(renderData());
+//     };
+// renderData();
+
+
+console.log(amount.value);
